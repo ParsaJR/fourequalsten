d12e20a8623d84b5363d876b93bd425fa60717a9
ParsaJR <parsajr@proton.me>
2024-07-15 17:59:49 +0330

    WIP

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <main class="h-screen main-linear-bg-mobile sm:main-linear-bg-desktop flex flex-col">
+      <section class="h-[40%]">
+        <div class="flex flex-row gap-10 justify-between m-2">
+          <UButton icon="i-heroicons-bars-3-16-solid" size="md" variant="ghost" color="white"/>
+          <div><span>0</span><span>/</span><span>20</span></div>
+        </div>
+      </section>
+      <section class="h-[60%]"></section>
+    </main>
   </div>
 </template>
+<style></style>
---
+++ components/Mountains.vue
@@ -0,0 +1,14 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg"
+        id="screenshot-59f3bd79-fd45-80bc-8004-a65d0d506373" viewBox="-384 240 1922 418"
+        style="-webkit-print-color-adjust::exact" xmlns:xlink="http://www.w3.org/1999/xlink" fill="none" version="1.1">
+        <g id="shape-59f3bd79-fd45-80bc-8004-a65d0d506373" data-testid="Path">
+            <g class="fills" id="fills-59f3bd79-fd45-80bc-8004-a65d0d506373">
+                <path
+                    d="M-380.995,358.548L-212.732,302.027L-148.632,240.000L119.788,320.011C119.788,320.011,732.780,262.256,956.819,262.033C995.872,261.994,1023.106,263.703,1033.216,267.771C1101.322,295.176,1535.000,296.888,1535.000,296.888L1538.000,658.000L-384.000,654.000L-380.995,358.548ZM956.819,262.033"
+                    style="fill:#020919;fill-opacity:1">
+                </path>
+            </g>
+        </g>
+    </svg>
+</template>
