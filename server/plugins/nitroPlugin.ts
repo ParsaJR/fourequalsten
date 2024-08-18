bf436c6f7378679b76aa5c173ad4ce4d10671991
ParsaJR <parsajr@proton.me>
2024-08-18 11:20:24 +0330

    adding some solvable levels

--- server/plugins/nitroPlugin.ts
+++ server/plugins/nitroPlugin.ts
@@ -19,9 +19,9 @@ async function populateLevels() {
     await Level.insertMany([
       { level: 1, levelNumbers: [1, 2, 3, 4] },
       { level: 2, levelNumbers: [9, 1, 5, 5] },
-      { level: 3, levelNumbers: [7, 1, 1, 1] },
-      { level: 4, levelNumbers: [9, 9, 9, 9] },
-      { level: 5, levelNumbers: [9, 9, 9, 9] },
+      { level: 3, levelNumbers: [2, 2, 2, 5] },
+      { level: 4, levelNumbers: [2, 5, 1, 7] },
+      { level: 5, levelNumbers: [6, 2, 6, 5] },
     ]);
   } catch (error) {
     console.log(error);
