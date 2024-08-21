3daa7db9cadd499603cb4c9036c1035a60c1d566
ParsaJR <parsajr@proton.me>
2024-08-21 14:36:52 +0330

    disable light mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxt/fonts", "nuxt-auth-utils"],
+  colorMode: {
+    preference: 'dark'
+  },
   runtimeConfig: {
     mongodbUri: process.env.MONGO_DB_CONNECTION_STRING,
   },
