c804b020f585ac469961eb8acf009a18ca1c6c32
ParsaJR <parsajr@proton.me>
2024-08-28 11:40:53 +0330

    title & description ( SEO )

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Maketen",
+      meta: [
+        {
+          name: "description",
+          content:
+            "Maketen is a casual number puzzle, where the goal is to make the 4 numbers in each level equal 10 by using the math symbols provided to us.",
+        },
+      ],
+    },
+  },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxt/fonts", "nuxt-auth-utils"],
